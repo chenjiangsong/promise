8fc5e3c150979cd438858c590a91e514609a17e3
chenjiangsong <274815408@qq.com>
2017-03-14 19:39:04 +0800

    新增callbackList

--- index.js
+++ index.js
@@ -18,18 +18,61 @@ class Promise {
     }
 
     resolve(value) {
-        
+        console.log('resolve')
+        const promise = this
+        promise.status = 'resolved'
+        promise.data = value
+        setTimeout(() => {
+            while (promise.onResolvedCallback.length) {
+                const onResolved = promise.onResolvedCallback.shift()
+                onResolved(promise.data)
+            }
+        })
     }
 
     reject(reason) {
-
+        const promise = this
+        promise.status = 'rejected'
+        promise.data = reason
+        while (promise.onRejectedCallback.length) {
+            const onRejected = promise.onRejectedCallback.shift()
+            onRejected(promise.data)
+        }
     }
 
     then(onResolved, onRejected) {
+        const promise = this
+        console.log('then')
+        // if (promise.status === 'resolved') {
+        //     onResolved(promise.data)
+        // }
+        //
+        // if (promise.status === 'rejected') {
+        //     onRejected(promise.data)
+        // }
 
+        // if (promise.status === 'pending') {
+        // setTimeout(() => {
+            promise.onResolvedCallback.push(onResolved)
+            promise.onRejectedCallback.push(onRejected)
+        // })
+        // }
     }
 
     notify() {
 
     }
 }
+
+const then = {
+    resolved(value) {
+        console.log('正确的值是：', value)
+    },
+    rejected(reason) {
+        console.log('拒绝的理由是：', reason);
+    }
+}
+
+const a = new Promise((resolve, reject) => {
+    resolve(1)
+}).then(then.resolved, then.rejected)
